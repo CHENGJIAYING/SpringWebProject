3f5c1ff6c52ce710a2f9bdf93eddb8f4667ed969
jiaying <vison919@gmail.com>
2022-06-10 13:39:55 +0800

    202206101339

--- src/main/java/spring/model/HouseService.java
+++ src/main/java/spring/model/HouseService.java
@@ -1,5 +1,6 @@
 package spring.model;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,7 @@ public class HouseService {
     public House findById(int houseid) throws SQLException {
     	return hDao.findById(houseid);
     }
-    
+    public List<House> findAll() {
+    	return hDao.findAll();
+    }
 }
